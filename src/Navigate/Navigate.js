be9abd099b65e13da9c52e2fc6ce7c8b3fffb94c
Vivek Mondal <89126640+Vivekmondal9@users.noreply.github.com>
2023-01-16 11:15:30 +0530

    Add files via upload
    There was a spelling Mistake in Navigate section , Now it has been fixed

---
+++ src/Navigate/Navigate.js
@@ -0,0 +1,37 @@
+import React from "react";
+import "./Navigate.css";
+import { useNavigate } from "react-router-dom";
+
+function Navigate(){
+    let nav=useNavigate();
+    function navtomen(){
+        nav("/mencloth",true)
+
+    }
+    function allProduct(){
+        nav("/",true);
+
+    }
+    function electronics(){
+        nav("/electronics",true);
+    }
+    function navtowomen(){
+        nav("/womencloth",true);
+
+    }
+    function navtoJewel(){
+        nav("/jewelery",true);
+    }
+    return(
+        <div className="navigate-part">
+            <span className="all" onClick={allProduct}>All</span>
+            <span className="electronics" onClick={electronics}>Electroncs</span>
+            <span className="jewelery" onClick={navtoJewel}>Jewelery</span>
+            <span className="men-cloth" onClick={navtomen}>Men's Clothing</span>
+            <span className="women-cloth" onClick={navtowomen}>Women's Clothing</span>
+        </div>
+    )
+}
+
+
+export default Navigate;
---
+++ src/Product/SingleProduct.js
@@ -0,0 +1,189 @@
+import React from "react";
+import { useNavigate } from "react-router";
+import Footer from "../Footer/Footer";
+import Header from "../Header/Header";
+import Loading from "./Loading";
+import "./SingleProduct.css";
+
+
+
+
+function Singleproduct(){
+
+
+    let singleprd=localStorage.getItem("selectedproduct");
+    singleprd=JSON.parse(singleprd);
+
+    function handleSubmit(e) {
+        // let prd;
+        // console.log(e.target.id);
+      
+
+
+
+
+        let cartit = localStorage.getItem("shoplanecart");
+
+
+
+        if (!cartit) {
+            cartit = [];
+        }
+        else {
+            cartit = JSON.parse(cartit);
+        }
+
+        let flag = 0;
+        for (let i = 0; i < cartit.length; i++) {
+            if (JSON.stringify(cartit[i].id) == e.target.id) {
+                flag = 1;
+                cartit[i].QTY = cartit[i].QTY + 1;
+            }
+        }
+
+
+        if (flag == 0) {
+
+            for(let i=0;i<singleprd.length;i++){
+                if(singleprd[i].id==e.target.id){
+            let record = JSON.stringify(singleprd[i]).substring(0, JSON.stringify(singleprd[i]).length - 1) + ",\"QTY\":1}";
+            let recordCart = JSON.parse(record);
+            cartit.push(recordCart);
+                }
+            }
+        }
+
+        localStorage.setItem("shoplanecart", JSON.stringify(cartit));
+        window.location.reload();
+
+
+    }
+      //geting the items from Favourite sec.
+      let fav = localStorage.getItem("favitems");
+      if (!fav) {
+          fav = []
+      }
+      else {
+          fav = JSON.parse(fav);
+      }
+
+
+      let nav=useNavigate();
+     // Function for favourite..
+     function favClicked(e) {
+         let point;
+ 
+         if (e.target.className == "fa fa-heart gray-color") {
+             e.target.className = "fa fa-heart red-color";
+             alert("Item added To Favourites.");
+             for(let i=0;i<singleprd.length;i++){
+                 if(singleprd[i].id==e.target.id){
+                     fav.push(singleprd[i]);
+                 }
+             }
+ 
+             localStorage.setItem("favitems",JSON.stringify(fav));
+ 
+         }
+         else {
+             e.target.className = "fa fa-heart gray-color";
+             alert("Item remove from Favourites.");
+             for(let i=0;i<fav.length;i++){
+                 if(e.target.id==fav[i].id){
+                     point=i;
+                     break;
+ 
+                 }
+             }
+ 
+             fav.splice(point,1);
+             localStorage.setItem("favitems",JSON.stringify(fav));
+ 
+ 
+         }
+     }
+
+    
+    let loginDetails;
+    loginDetails = localStorage.getItem("logdata");
+    loginDetails = JSON.parse(loginDetails);
+
+
+    function pleaselog(){
+        alert("Please Login First!!");
+        nav("/login",true)
+    }
+    return(
+        <div>
+            <Header></Header>
+            <div className="row">
+                {singleprd &&
+                    singleprd.map((product, i) =>
+                    (<div className="prd-main" key={i} id={product.id}>
+                         {!loginDetails &&
+                            <div className="fav-part" id={`fav-heart-${product.id}`}><i className="fa fa-heart gray-color" id={product.id} onClick={pleaselog}></i></div>
+                        }
+                        {loginDetails &&
+                            (<>{
+                                JSON.stringify(fav).includes(JSON.stringify(product)) ?
+                                    <div className="fav-part" key={i} id={`fav-heart-${product.id}`}><i className="fa fa-heart red-color" id={product.id} onClick={favClicked}></i></div>
+                                    :
+
+                                    <div className="fav-part" key={i} id={`fav-heart-${product.id}`}><i className="fa fa-heart gray-color" id={product.id} onClick={favClicked}></i></div>
+
+                            }</>)}
+
+                        <img className="prd-img-top"src={product.image} alt="Loading Image"></img>
+                        <h5 className="prd-title">{product.title}</h5>
+                        <p>{product.description}</p>
+                        <div className="prd-ratings">
+
+                            <div className="prd-rating-star">
+                                {Math.floor(product.rating.rate) == 0 &&
+                                    (<><span class="fa fa-star "></span><span class="fa fa-star"></span><span class="fa fa-star"></span><span class="fa fa-star"></span><span class="fa fa-star"></span></>
+                                    )}
+                                {Math.floor(product.rating.rate) == 1 &&
+                                    (<><span class="fa fa-star checked"></span><span class="fa fa-star"></span><span class="fa fa-star"></span><span class="fa fa-star"></span><span class="fa fa-star"></span></>
+                                    )}
+                                {Math.floor(product.rating.rate) == 2 &&
+                                    (<><span class="fa fa-star checked"></span><span class="fa fa-star checked"></span><span class="fa fa-star"></span><span class="fa fa-star"></span><span class="fa fa-star"></span></>
+                                    )}
+                                {Math.floor(product.rating.rate) == 3 &&
+                                    (<><span class="fa fa-star checked"></span><span class="fa fa-star checked"></span><span class="fa fa-star checked"></span><span class="fa fa-star"></span><span class="fa fa-star"></span></>
+                                    )}
+                                {Math.floor(product.rating.rate) == 4 &&
+                                    (<><span class="fa fa-star checked"></span><span class="fa fa-star checked"></span><span class="fa fa-star checked"></span><span class="fa fa-star checked"></span><span class="fa fa-star"></span></>
+                                    )}
+                                {Math.floor(product.rating.rate) == 5 &&
+                                    (<><span class="fa fa-star checked"></span><span class="fa fa-star checked"></span><span class="fa fa-star checked"></span><span class="fa fa-star checked"></span><span class="fa fa-star checked"></span></>
+                                    )}
+
+                            </div>
+                            <span>{product.rating.count} Reviews</span>
+                        </div>
+                        <div className="prd-price-sec">
+                            <p className="prd-price">$ {product.price}</p>
+                        </div>
+                        <button className="prd-add-cart" onClick={handleSubmit} id={product.id}><i className='fa fa-shopping-cart crt' />Add-to-cart</button>
+
+
+                    </div>)
+                    )}
+                {
+                    singleprd.length == 0 &&
+                    (<div className="loading"><Loading></Loading></div>)
+                }
+
+            </div>
+
+
+            <Footer></Footer>
+        </div>
+    )
+
+}
+
+
+
+
+export default Singleproduct;
